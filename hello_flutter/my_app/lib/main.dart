9ab6e4a7d743c7b00a0ddd512310ab07afeb914a
Bruno Alfred <alfredybruno@gmail.com>
2020-03-16 23:16:51 +0300

    scaffold widget

--- hello_flutter/my_app/lib/main.dart
+++ hello_flutter/my_app/lib/main.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My first App"),
+          title: Text("My First App"),
           centerTitle: true,
         ),
-        body: Center(child: Text("Hello Bruno"),)
+        body: Center(
+          child: Text("Hello Bruno"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("click!"),
+        ),
       ),
     ));
