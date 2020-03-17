7516c00dccb403db08af7ecbda1680e29ce7a97a
Bruno Alfred <alfredybruno@gmail.com>
2020-03-17 12:57:46 +0300

    layouts in flutter

--- hello_flutter/intro_layouts/lib/main.dart
+++ hello_flutter/intro_layouts/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import './ui/home.dart';
 
 void main() => runApp(new MaterialApp(
-      color: Colors.greenAccent,
       title: "Layouts",
       home: new Home(),
     ));
--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -7,10 +7,41 @@ class Home extends StatelessWidget {
     return new Container(
       color: Colors.greenAccent,
       alignment: Alignment.center,
-      child: new Text(
-        "Hello Container",
-        textDirection: TextDirection.ltr,
-        style: new TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            "Hello Container",
+            textDirection: TextDirection.ltr,
+            style: new TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 20.0),
+          ),
+          new Text(
+            "Welcome",
+            textDirection: TextDirection.rtl,
+            style: TextStyle(
+                color: Colors.cyan,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 20.0),
+          ),
+          new Container(
+            alignment: Alignment.center,
+            child: new Text(
+              "Me",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
