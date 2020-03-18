65a961d286ee67bdec71d55fc83222d0434e8c87
Bruno Alfred <alfredybruno@gmail.com>
2020-03-18 22:07:11 +0300

    statefull widget exploring

--- hello_flutter/flutter_app/lib/main.dart
+++ hello_flutter/flutter_app/lib/main.dart
@@ -1,43 +1,6 @@
-// This is the main function which is excute at first,
-// it doesnt return anything "voidType"
-// importing the packages to be used
 import 'package:flutter/material.dart';
+import './ui/home.dart';
 
-void main() => runApp(MyApp());
-
-class 
-
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('EasyList'),
-            ),
-            body: Column(
-              children: [
-                widget(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(100.0),
-                      child: RaisedButton(
-                      onPressed: () {},
-                      child: Text('Add Msosi'),
-                    ),,)
-                 
-                    Card(
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset("assets/food.jpg"),
-                          Text('Food Paradise')
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )));
-  }
-}
+void main() => runApp(new MaterialApp(
+      home: Home(),
+    ));
---
+++ hello_flutter/flutter_app/lib/ui/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      alignment: Alignment.center,
+      color: Colors.teal,
+    );
+  }
+}
