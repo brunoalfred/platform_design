86fc8a9b3ca8d8aa78961203ec72132bd4c87f44
Bruno Alfred <alfredybruno@gmail.com>
2020-03-25 18:43:27 +0300

    adding a dialog button

--- hello_flutter/my_app/lib/ui/home.dart
+++ hello_flutter/my_app/lib/ui/home.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/raised_button.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        backgroundColor: Colors.greenAccent,
-        title: new Center(
-          child: new Text(
-            "welcome",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 20.0,
-              fontFamily: "Roboto",
-              fontWeight: FontWeight.w500,
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
+          title: new Center(
+            child: new Text(
+              "welcome",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 20.0,
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
         ),
-      ),
-      body: new Center(
-        child: new Text(
-          "Hello !",
-          style: TextStyle(
-              color: Colors.green,
-              fontFamily: "Roboto",
-              fontWeight: FontWeight.w500,
-              fontSize: 20.0),
-        ),
-      ),
-      HelloButton();
-    );
+        body: new Column(
+          children: <Widget>[
+            new Center(
+              child: new Text(
+                "Hello!!!",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            HelloButton(),
+          ],
+        ));
   }
 }
 
@@ -41,16 +46,29 @@ class HelloButton extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Container(
+      margin: EdgeInsets.only(top: 50.0),
       alignment: Alignment.center,
       child: new RaisedButton.icon(
-        onPressed: () {},
+        onPressed: () {
+          enjoyButton();
+        },
         icon: Icon(
           Icons.favorite,
           color: Colors.red,
           size: 20.0,
         ),
         elevation: 6.0,
+        label: Text("hello"),
       ),
     );
   }
+}
+
+void enjoyButton(BuildContext context) {
+  var alertDialog = new AlertDialog(
+    content: Text("Woow"),
+    title: Text("loved"),
+  );
+
+  showDialog(context: context, builder: (BuildContext context))
 }
