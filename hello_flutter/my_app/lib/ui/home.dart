2ceff44733a654d6d2d8b97dab17c9a05503ebe0
Bruno Alfred <alfredybruno@gmail.com>
2020-03-26 16:15:17 +0300

    stateful Widget

--- hello_flutter/my_app/lib/ui/home.dart
+++ hello_flutter/my_app/lib/ui/home.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 
-class _Home extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _HomeState();
   }
-
 }
 
-class _HomeState extends State<_Home> {
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 20.0,
+        backgroundColor: Colors.blueAccent,
+        title: new Center(
+          child: Text(
+            "click Me",
+            overflow: TextOverflow.fade,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        brightness: Brightness.dark,
+      ),
+      body: new Container(
+        alignment: Alignment.center,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              child: new Text(
+                "Welcome",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 30.0, fontFamily: ''),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
