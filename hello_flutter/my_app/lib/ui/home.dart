5e58c763096b9176232a7190d0552151da41f07e
Bruno Alfred <alfredybruno@gmail.com>
2020-03-26 15:52:31 +0300

    stateful Widget

--- hello_flutter/my_app/lib/ui/home.dart
+++ hello_flutter/my_app/lib/ui/home.dart
@@ -1,50 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class _Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+
+}
+
+class _HomeState extends State<_Home> {
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text(
-          "Push Button",
-          style: TextStyle(
-            fontFamily: "Roboto",
-            fontSize: 20.0,
-            fontWeight: FontWeight.w500,
-            color: Colors.white,
-          ),
-          textAlign: TextAlign.right,
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-      body: new Container(
-        alignment: Alignment.center,
-        color: Colors.white70,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              alignment: Alignment.center,
-              width: 390.0,
-              height: 70.0,
-              child: Text(
-                "welcome",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 40.0,
-                  fontFamily: 'Baloon Da 2',
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            new Container(),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
+
 }
