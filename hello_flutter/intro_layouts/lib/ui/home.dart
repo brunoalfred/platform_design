a232ddbbeb21c42fde34678c82f3e362692e8f5c
Bruno Alfred <alfredybruno@gmail.com>
2020-03-27 09:06:09 +0300

    setState implemented

--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -24,29 +24,30 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: new Container(
-          alignment: Alignment.center,
-          child: new Text("Change VAlue"),
-        ),
-      ),
-      body: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Container(
-            child: new Text(_valueSet),
+        appBar: AppBar(
+          title: new Container(
+            alignment: Alignment.center,
+            child: new Text("Change VAlue"),
           ),
-          new RaisedButton(
-            onPressed: _onPress,
-            elevation: 9.0,
-            color: Colors.blueAccent,
-            child: Icon(
-              Icons.find_replace,
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
-    );
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                child: new Text(_valueSet),
+              ),
+              new RaisedButton(
+                onPressed: _onPress,
+                elevation: 9.0,
+                color: Colors.blueAccent,
+                child: Icon(
+                  Icons.find_replace,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
