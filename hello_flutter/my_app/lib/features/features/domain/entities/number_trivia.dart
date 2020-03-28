d7d2d22fd2e7dbde58868b98e440724cb1d93fb3
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 00:04:06 +0300

    entities

---
+++ hello_flutter/my_app/lib/features/features/domain/entities/number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  NumberTrivia({
+    @required this.text,
+    @required this.number,
+  }) : super([text, number]);
+}
