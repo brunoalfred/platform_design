45fa871c33ac00cba0b2cf7ddbbbfe036ed28ffc
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 11:15:43 +0300

    implementing my GetConcreteNUmberTrivia class

---
+++ hello_flutter/my_app/lib/features/features/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:my_app/features/features/domain/repositories_interfaces/number_trivia_repo.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
