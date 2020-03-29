dced4267eeb6675aa842aebde0aefde34d2504e8
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 11:30:55 +0300

    importing a file to fix the REDS

--- hello_flutter/my_app/test/domain/usecases/get_concrete_number_trivia_test.dart
+++ hello_flutter/my_app/test/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,7 +1,8 @@
-import 'dart:html';
+import 'package:mockito/mockito.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_app/features/features/domain/repositories_interfaces/number_trivia_repo.dart';
+import 'package:my_app/features/features/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
