c7f73554a0910c743781d1b7477fbd42aecff46b
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 23:29:53 +0300

    sketching the appBarr

--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
 
-class 
+class NoteList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: new Center(
+          child: new Container(
+            child: new Text(
+              "List Of Notes",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                backgroundColor: Colors.blueAccent,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
