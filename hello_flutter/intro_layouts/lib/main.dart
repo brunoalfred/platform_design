0fa2b6132d55c33836075100f2f4d8b4be671a7b
Bruno Alfred <alfredybruno@gmail.com>
2020-03-29 23:24:25 +0300

    editing the main.dart file

--- hello_flutter/intro_layouts/lib/main.dart
+++ hello_flutter/intro_layouts/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import './ui/home.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sharpening Skills',
-      home: Home(),
+      title: "REST API",
+      home: NoteLIst(),
     ));
+
