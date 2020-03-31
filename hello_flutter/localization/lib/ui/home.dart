06e3b6e4ac172f4690bf651b5a07ce2054dedce4
Bruno Alfred <alfredybruno@gmail.com>
2020-03-31 12:50:33 +0300

    implementing the Localization Class

--- hello_flutter/localization/lib/ui/home.dart
+++ hello_flutter/localization/lib/ui/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -36,4 +41,16 @@ class AppLocalizations {
   static AppLocalizations of(BuildContext context) {
     return Localizations.of<AppLocalizations>(context, AppLocalizations);
   }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    String jsonString =
+        await rootBundle.loadString("language/${locale.languageCode}.json");
+  Map<String, DynamicLibrary> jsonMap = json.decode(jsonString);
+  
+  _localizedStrings = jsonMap.map((key, value))}
 }
