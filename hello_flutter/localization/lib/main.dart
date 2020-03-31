ec5e828122ce69c6d42e367d4c9a577af5f2bf57
Bruno Alfred <alfredybruno@gmail.com>
2020-03-31 13:41:54 +0300

    adding documentation on the project

--- hello_flutter/localization/lib/main.dart
+++ hello_flutter/localization/lib/main.dart
@@ -6,21 +6,26 @@ void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Localized App',
       home: Home(),
+
+      /* These delegates make sure that the localization
+       data for the proper language is loaded */
+
       localizationsDelegates: [
         // THIS CLASS WILL BE ADDED LATER
         // A class which loads the translations from json files
-        // 
+        //
         AppLocalizations.delegate,
         // Built-In localization of basics text for Material Widgets
         GlobalMaterialLocalizations.delegate,
         // BUilt-In localizations for text direction LTR/RTL
         GlobalWidgetsLocalizations.delegate,
       ],
+      // List all of the supported locales here
       supportedLocales: [
-        // 
         const Locale("en", "US"),
         const Locale("sw", ""),
       ],
+
       localeResolutionCallback: (locale, supportedLocales) {
         //Check if the current device locale is supported
         for (var supportedLocale in supportedLocales) {
