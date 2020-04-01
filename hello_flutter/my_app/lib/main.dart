4b0e41e15b481c2d306480141dbb4f1974628060
Bruno Alfred <alfredybruno@gmail.com>
2020-04-01 20:15:12 +0300

    implementing the BLOC

--- hello_flutter/my_app/lib/main.dart
+++ hello_flutter/my_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'ui/home.dart';
+
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
       title: "Login",
+      theme: ThemeData(primarySwatch: Colors.orange,),
       home: Home(),
     ));
