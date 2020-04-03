8c116bddb24ef93dd0615b4bc256e0c03fba26d8
Bruno Alfred <alfredybruno@gmail.com>
2020-04-03 12:40:39 +0300

    making a network request documenting

---
+++ hello_flutter/flutter_app/lib/models/response_from_request.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+import './network_request.dart';
+
+/* after fetching data from the internet, we have to convert them to 
+Dart OBJECTS. For easier working with the DATA*/
+
+
