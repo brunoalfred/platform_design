2492ab541295efc44bd34e8ba8dc807c39044627
Bruno Alfred <alfredybruno@gmail.com>
2020-04-03 12:35:17 +0300

    making a network request documenting

--- hello_flutter/flutter_app/lib/models/network_request.dart
+++ hello_flutter/flutter_app/lib/models/network_request.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 
+// making a network request 
 Future<http.Response> fetchPhotos(http.Client client) async {
   return client.get ('https://jsonplaceholder.typicode.com/photos');
 }
