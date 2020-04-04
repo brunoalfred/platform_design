5badbfe9fde445455801e778955ec2a84f6595e3
Bruno Alfred <alfredybruno@gmail.com>
2020-04-05 01:35:37 +0300

    validating and saving data

--- hello_flutter/intro_layouts/lib/main.dart
+++ hello_flutter/intro_layouts/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intro_layouts/ui/home.dart';
+import 'package:intro_layouts/ui/login.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
