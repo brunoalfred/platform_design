479157d419bb85d36ad23e45fd0b1b7d99c99c28
Bruno Alfred <alfredybruno@gmail.com>
2020-04-04 16:36:30 +0300

    creating a form

--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(centerTitle: true,title: new Text("Form"),),);
+  }
+
 }
