558ba659a94c095c703128d821dc6d3ca39383f3
Bruno Alfred <alfredybruno@gmail.com>
2020-04-04 15:54:34 +0300

    creating a widhet

--- hello_flutter/intro_layouts/lib/main.dart
+++ hello_flutter/intro_layouts/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import './ui/home.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "REST API",
-      home: NoteList(),
+      title: "Inputs",
+      home: Home(),
     ));
