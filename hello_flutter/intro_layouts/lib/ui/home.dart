4a25110b009fc27b493bc06366af213c26a0ca10
Bruno Alfred <alfredybruno@gmail.com>
2020-04-06 09:53:53 +0300

    http request plugin in flutter

--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -30,8 +30,10 @@ class _HomeState extends State<Home> {
 
     setState(() {
       var convertDataToJson = json.decode(response.body);
-      data = 
+      data = convertDataToJson["result"];
     });
+
+    return "success";
   }
 
   @override
