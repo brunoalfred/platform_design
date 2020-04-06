074fd33bdec322fca6f2aed45fc84641551ecbc3
Bruno Alfred <alfredybruno@gmail.com>
2020-04-06 10:41:31 +0300

    http request plugin in flutter

--- hello_flutter/intro_layouts/lib/http/posts_model.dart
+++ hello_flutter/intro_layouts/lib/http/posts_model.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-class Post  {
+class Post {
   final int userId;
   final int id;
-  final String 
+  final String title;
+  final String body;
+
+  Post({
+    @required this.userId,
+    @required this.id,
+    @required this.title,
+    @required this.body,
+  });
+  factory Post.fromJson() {
+    
+  }
 }
+
+
