eaf9c0b978c16c37592e128e372972a19009535a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-06 14:38:46 +0300

    http request plugin in flutter

---
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:intro_layouts/http/http_service.dart';
+
+class Home extends StatelessWidget {
+  final  HttpService httpService = HttpService();
+
+  
+}
