3a2443dda63e02aa63364348ef9fd29dc3381bb5
Bruno Alfred <alfredybruno@gmail.com>
2020-04-06 15:34:59 +0300

    http request plugin in flutter

--- hello_flutter/intro_layouts/lib/ui/home.dart
+++ hello_flutter/intro_layouts/lib/ui/home.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:intro_layouts/http/http_service.dart';
-import 'package:intro_layouts/http/posts_model.dart';
 
 class Home extends StatelessWidget {
-  final HttpService httpService = HttpService();
-
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: new Text('Http Request'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: new Text(
+          "Http Request",
         ),
-        body: FutureBuilder(builder:
-            (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
-          if (snapshot.hasData) {
-            List<Post> posts = snapshot.data;
-          return ListView (children: posts.map(Post post) => ListTile(),);}
-        }));
+        centerTitle: true,
+      ),
+      body: FutureBuilder(builder: (null)),
+    );
   }
 }
