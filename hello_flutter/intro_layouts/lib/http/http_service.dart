960e9b21e63bea014a34081df7e2b333ac7d9965
Bruno Alfred <alfredybruno@gmail.com>
2020-04-06 10:50:15 +0300

    http request plugin in flutter

--- hello_flutter/intro_layouts/lib/http/http_service.dart
+++ hello_flutter/intro_layouts/lib/http/http_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:intro_layouts/http/posts_model.dart';
 
@@ -6,5 +8,17 @@ class HttpService {
 
   Future<List<Post>> getPost() async {
     Response response = await get(url);
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+
+      List<Post> posts = body
+          .map(
+            (dynamic item) => Post.fromJson(item),
+          )
+          .toList();
+
+        return posts;
+    }
   }
 }
