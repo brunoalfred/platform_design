0634e9a71b90f3e6bb87322c7facfe17ee551c16
Bruno Alfred <alfredybruno@gmail.com>
2020-04-09 13:09:26 +0300

    creating the post class

--- hello_flutter/rest_api/lib/http_services.dart
+++ hello_flutter/rest_api/lib/http_services.dart
@@ -1,5 +1,11 @@
+import 'dart:html';
+
 import 'package:http/http.dart' as http;
 
 class HttpService {
   final String url = 'https://jsonplaceholder.typicode.com/posts';
+
+  Future<List<Post>> getPosts() async{
+    http.Response response = await http.get(url);
+  }
 }
