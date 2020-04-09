774e4dee47c42c057296fa2d8fc484c918c22b79
Bruno Alfred <alfredybruno@gmail.com>
2020-04-09 12:25:05 +0300

    home dart file

--- hello_flutter/rest_api/lib/main.dart
+++ hello_flutter/rest_api/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,title: 'Rest Api Consuming',
+      debugShowCheckedModeBanner: false,
+      title: 'Rest Api Consuming',
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        brightness: Brightness.dark,
+      ),
     ));
