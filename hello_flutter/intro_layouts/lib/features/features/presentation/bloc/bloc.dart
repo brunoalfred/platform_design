072c49eb6b6894f6e0837a34c5a92fec3374c904
Bruno Alfred <alfredybruno@gmail.com>
2020-04-09 11:47:14 +0300

    deleted projects

--- hello_flutter/intro_layouts/lib/features/features/presentation/bloc/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'features_bloc.dart';
-export 'features_event.dart';
-export 'features_state.dart';
--- hello_flutter/intro_layouts/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:intro_layouts/ui/home.dart';
-
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Inputs",
-      home: Home(),
-    ));
