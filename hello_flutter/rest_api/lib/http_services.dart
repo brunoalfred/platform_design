ed2865e183b1df0d6f7dbbc1be47ed12299b3dbc
Bruno Alfred <alfredybruno@gmail.com>
2020-04-09 13:19:23 +0300

    decoding the response form the http request sent

--- hello_flutter/rest_api/lib/http_services.dart
+++ hello_flutter/rest_api/lib/http_services.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
 import 'dart:html';
 
 import 'package:http/http.dart' as http;
 
+import 'post_models.dart';
+
 class HttpService {
   final String url = 'https://jsonplaceholder.typicode.com/posts';
 
-  Future<List<Post>> getPosts() async{
+  Future<List<Post>> getPosts() async {
     http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+
+      
+    }
+
+
   }
 }
