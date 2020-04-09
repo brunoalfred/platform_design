ebe22f12f724026ddf8c27a235108d3b3d353488
Bruno Alfred <alfredybruno@gmail.com>
2020-04-09 13:14:00 +0300

    creating a constructor

--- hello_flutter/rest_api/lib/post_models.dart
+++ hello_flutter/rest_api/lib/post_models.dart
@@ -1,1 +1,14 @@
-class Post {}
+import 'package:flutter/foundation.dart';
+
+class Post {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  Post(
+      {@required this.id,
+      @required this.userId,
+      @required this.title,
+      @required this.body});
+}
