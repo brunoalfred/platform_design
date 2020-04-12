c024180180251a3523d09ca8b7ed43beaf2f20af
Bruno Alfred <alfredybruno@gmail.com>
2020-04-12 18:19:09 +0300

    cleaned the main file

--- hello_flutter/flutter_app/lib/ui/home.dart
+++ hello_flutter/flutter_app/lib/ui/home.dart
@@ -1,8 +1,45 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Navigation In flutter',
-      theme: ThemeData(
-          accentColor: Colors.blueAccent, brightness: Brightness.light),
-    ));
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text('First Screen'),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondRoute()));
+          },
+          child: new Text("Open Route"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Second Route"),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: new Text("Go Back!"),
+        ),
+      ),
+    );
+  }
+}
