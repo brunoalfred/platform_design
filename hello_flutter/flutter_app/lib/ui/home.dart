c7a2aeb0e5e225a382e789082517873412c4d053
Bruno Alfred <alfredybruno@gmail.com>
2020-04-12 18:17:25 +0300

    making a theme on the main.dart

---
+++ hello_flutter/flutter_app/lib/ui/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation In flutter',
+      theme: ThemeData(
+          accentColor: Colors.blueAccent, brightness: Brightness.light),
+    ));
