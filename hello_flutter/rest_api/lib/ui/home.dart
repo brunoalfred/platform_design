f1af6f60f294e568414d0aa16095c4a502712a90
Bruno Alfred <alfredybruno@gmail.com>
2020-04-12 15:40:42 +0300

    defined two new lists

--- hello_flutter/rest_api/lib/ui/home.dart
+++ hello_flutter/rest_api/lib/ui/home.dart
@@ -8,6 +8,30 @@ class Home extends StatefulWidget {
   }
 }
 
+final titles = [
+  'bike',
+  'boat',
+  'bus',
+  'car',
+  'railway',
+  'run',
+  'subway',
+  'transit',
+  'walk'
+];
+
+final icons = [
+  Icons.directions_bike,
+  Icons.directions_boat,
+  Icons.directions_bus,
+  Icons.directions_car,
+  Icons.directions_railway,
+  Icons.directions_run,
+  Icons.directions_subway,
+  Icons.directions_transit,
+  Icons.directions_walk
+];
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
