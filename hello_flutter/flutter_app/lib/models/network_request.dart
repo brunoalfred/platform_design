88f6a5995eb9cfb3d170d94aaa79d052a9c5abe0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-12 17:45:26 +0300

    cleaned and started afresh

--- hello_flutter/flutter_app/lib/models/network_request.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-// making a network request 
-Future<http.Response> fetchPhotos(http.Client client) async {
-  return client.get ('https://jsonplaceholder.typicode.com/photos');
-}
-
