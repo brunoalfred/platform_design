e9996e702618125017ce95781e7693386cd43462
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 11:31:33 +0300

    routes

--- hello_flutter/flutter_app/lib/main.dart
+++ hello_flutter/flutter_app/lib/main.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Navigation In flutter',
+      theme: ThemeData(
+        accentColor: Colors.greenAccent,
+        brightness: Brightness.light,
+      ),
     ));
