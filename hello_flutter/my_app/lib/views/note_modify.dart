a1c8e93687720e9a0f51d9846a0a3b8d735a6bec
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 16:56:56 +0300

    just implemented a new page

---
+++ hello_flutter/my_app/lib/views/note_modify.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NoteModify extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
