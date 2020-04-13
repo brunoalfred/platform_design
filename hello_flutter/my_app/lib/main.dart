88b1c71bedf2003a4ed15f0e8352017aed49801b
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 15:41:03 +0300

    themedata of the app

--- hello_flutter/my_app/lib/main.dart
+++ hello_flutter/my_app/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:my_app/views/home.dart';
 void main() => runApp(MaterialApp(
       title: 'Rest API consuming',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        brightness: Brightness.light,
+      ),
       home: ListNote(),
     ));
