f89fda49db183fb3937f38321d4f62a1c9a128f3
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 15:56:54 +0300

    cleaned the workspace

--- hello_flutter/my_app/lib/views/home.dart
+++ hello_flutter/my_app/lib/views/home.dart
@@ -14,26 +14,7 @@ class ListNote extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: new Center(
-        child: new ListView.separated(
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: new Text(
-                  "data",
-                  textAlign: TextAlign.center,
-                ),
-                subtitle: new Text(
-                  "Date Generated",
-                  textAlign: TextAlign.left,
-                ),
-              );
-            },
-            separatorBuilder: (_, __) => Divider(
-                  height: 1.0,
-                  color: Theme.of(context).primaryColor,
-                ),
-            itemCount: 20),
-      ),
+      body: 
     );
   }
 }
