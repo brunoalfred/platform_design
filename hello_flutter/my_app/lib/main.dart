a3c2c67c2e5adfc7f1c054346bbc2d4588740e46
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 15:17:58 +0300

    theme of APP

---
+++ hello_flutter/my_app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Rest API consuming',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+    ));
--- hello_flutter/my_app/lib/views/home.dart
+++ hello_flutter/my_app/lib/views/home.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ListNote extends StatelessWidget{
+class ListNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text(
+          'List of Notes',
+          style: new TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
