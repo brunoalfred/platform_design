494812029ce3e61701929713264ac0a6c5bc874a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 16:44:50 +0300

    implemented more notes

--- hello_flutter/my_app/lib/views/home.dart
+++ hello_flutter/my_app/lib/views/home.dart
@@ -32,35 +32,66 @@ class ListNote extends StatelessWidget {
       createDateTime: DateTime.now(),
       lastEditDateTime: DateTime.now(),
     ),
+    new NoteForListing(
+      noteId: "6",
+      noteTitle: "Note 6",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "7",
+      noteTitle: "Note 7",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "8",
+      noteTitle: "Note 8",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "9",
+      noteTitle: "Note 9",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "10",
+      noteTitle: "Note 10",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: new Text(
-            'List of Notes',
-            style: new TextStyle(
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        title: new Text(
+          'List of Notes',
+          style: new TextStyle(
+            color: Colors.white,
           ),
-          centerTitle: true,
         ),
-        body: new Container(
-          child: new ListView.separated(
-              itemBuilder: (contex, index) {
-                return ListTile(
-                  title: new Text(notes[index].noteTitle),
-                  subtitle: new Text(
-                      'Last edited date is ${notes[index].lastEditDateTime}'),
-                );
-              },
-              separatorBuilder: (_, __) => Divider(
-                    height: 1.0,
-                    color: Theme.of(context).primaryColor,
-                  ),
-              itemCount: notes.length),
-        ));
+        centerTitle: true,
+      ),
+      body: new Container(
+        child: new ListView.separated(
+            itemBuilder: (contex, index) {
+              return ListTile(
+                title: new Text(notes[index].noteTitle),
+                subtitle: new Text(
+                    'Last edited date is ${notes[index].lastEditDateTime}'),
+              );
+            },
+            separatorBuilder: (_, __) => Divider(
+                  height: 1.0,
+                  color: Theme.of(context).primaryColor,
+                ),
+            itemCount: notes.length),
+      ),
+    );
   }
 }
