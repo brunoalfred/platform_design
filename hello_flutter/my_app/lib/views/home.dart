f5da452c4c82d23f733a51f81e38f16d47e9387d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 16:00:44 +0300

    re implemnted the widget

--- hello_flutter/my_app/lib/views/home.dart
+++ hello_flutter/my_app/lib/views/home.dart
@@ -5,16 +5,25 @@ class ListNote extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: new Text(
-          'List of Notes',
-          style: new TextStyle(
-            color: Colors.white,
+        appBar: AppBar(
+          title: new Text(
+            'List of Notes',
+            style: new TextStyle(
+              color: Colors.white,
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: 
-    );
+        body: new Container(
+          child: new ListView.separated(
+              itemBuilder: (contex, index) {
+                return ListTile();
+              },
+              separatorBuilder: (_, __) => Divider(
+                    height: 1.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+              itemCount: 20),
+        ));
   }
 }
