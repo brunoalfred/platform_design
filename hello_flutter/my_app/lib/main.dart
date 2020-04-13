27944a771349ae4c719a55deee58920c97cd54c0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 15:19:02 +0300

    imported the ListNote class

--- hello_flutter/my_app/lib/main.dart
+++ hello_flutter/my_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/views/home.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Rest API consuming',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
+      home: ListNote(),
     ));
