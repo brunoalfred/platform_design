2c8f4c99025daf64882aaa874d0e2572d94a7134
Bruno Alfred <alfredybruno@gmail.com>
2020-04-13 16:34:15 +0300

    created sample data

--- hello_flutter/my_app/lib/views/home.dart
+++ hello_flutter/my_app/lib/views/home.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/models/note_for_listing.dart';
 
 class ListNote extends StatelessWidget {
-  final notes = [];
+  final notes = [
+    new NoteForListing(
+      noteId: "1",
+      noteTitle: "Note 1",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "2",
+      noteTitle: "Note 2",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+        noteId: "3",
+        noteTitle: "Note 3",
+        createDateTime: DateTime.now(),
+        lastEditDateTime: DateTime.now()),
+    new NoteForListing(
+      noteId: "4",
+      noteTitle: "Note 4",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+    new NoteForListing(
+      noteId: "5",
+      noteTitle: "Note 5",
+      createDateTime: DateTime.now(),
+      lastEditDateTime: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
