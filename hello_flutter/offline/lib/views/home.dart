cbc1580b55298ffe3903b585e071f48032aea3f2
Bruno Alfred <alfredybruno@gmail.com>
2020-04-16 08:51:47 +0300

    creating a home screen

---
+++ hello_flutter/offline/lib/views/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home>
