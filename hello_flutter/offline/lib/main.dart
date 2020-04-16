8e92357368f192f02b12975c648d6943315c9dc8
Bruno Alfred <alfredybruno@gmail.com>
2020-04-16 09:00:53 +0300

    naming the route

--- hello_flutter/offline/lib/main.dart
+++ hello_flutter/offline/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:offline/views/home.dart';
+import 'package:offline/views/login.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -8,6 +12,10 @@ void main() => runApp(
           accentColor: Colors.cyanAccent,
           brightness: Brightness.light,
         ),
-        initialRoute: '/',routes: <String, WidgetBuilder>{},
+        initialRoute: '/login',
+        routes: <String, WidgetBuilder>{
+          '/login': (context) => Login(),
+          '/home': (context) => Home()
+        },
       ),
     );
