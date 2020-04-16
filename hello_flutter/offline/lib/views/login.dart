4e32596adc62cc229519b2e543e6fcdad035bac1
Bruno Alfred <alfredybruno@gmail.com>
2020-04-16 08:49:37 +0300

    sduusiugvsaugcdspicidsuvgvvr7sufvds

---
+++ hello_flutter/offline/lib/views/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold();
+  }
+}
