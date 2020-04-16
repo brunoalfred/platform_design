f9f753004e69e25a4204a83e8923616465646434
Bruno Alfred <alfredybruno@gmail.com>
2020-04-16 08:44:22 +0300

    creating a login

--- hello_flutter/offline/lib/main.dart
+++ hello_flutter/offline/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'Connectivity',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        accentColor: Colors.cyanAccent,
-        brightness: Brightness.light,
-      ),home: Home(),
-    ));
+void main() => runApp(
+      MaterialApp(
+        title: 'Connectivity',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          accentColor: Colors.cyanAccent,
+          brightness: Brightness.light,
+        ),
+      ),
+    );
