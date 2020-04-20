d6fdb9cead82f703882de2725a566b0ffba1fdd6
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 22:40:49 +0300

    practicing some shortcuts

--- hello_flutter/offline/lib/main.dart
+++ hello_flutter/offline/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(MaterialApp (
-title: 'Trying Navigation App With ListView',debugShowCheckedModeBanner: false,theme: ThemeData(),
-) );
+void main() => runApp(MaterialApp(
+      title: 'Trying Navigation App With ListView',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Colors.indigoAccent,
+      ),
+    ));
