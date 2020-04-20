140261fd595d1319784f6b1d5e4042e0b6d9b01d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 22:34:52 +0300

    git ignore the metadata and packages files

--- hello_flutter/offline/lib/main.dart
+++ hello_flutter/offline/lib/main.dart
@@ -1,25 +1,6 @@
-
 import 'package:flutter/material.dart';
-import 'package:offline/views/home.dart';
-import 'package:offline/views/login.dart';
 
 
-
-void main() => runApp(
-      MaterialApp(
-        title: 'Connectivity',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          accentColor: Colors.cyanAccent,
-          brightness: Brightness.light,
-        ),
-        initialRoute: '/login',
-        routes: <String, WidgetBuilder>{
-          // ? create the Login page
-          '/login': (context) => Login(),
-          // ? create the home  page
-          '/home': (context) => Home(),
-        },
-      ),
-    );
-
+void main() => runApp(MaterialApp (
+title: 'Trying Navigation App With ListView',debugShowCheckedModeBanner: false,theme: ThemeData(),
+) );
