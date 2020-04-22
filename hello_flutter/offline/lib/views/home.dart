c8d093e890932691198f41f8695fc7ee3ee01cce
Bruno Alfred <alfredybruno@gmail.com>
2020-04-22 15:48:51 +0300

    the listTile

--- hello_flutter/offline/lib/views/home.dart
+++ hello_flutter/offline/lib/views/home.dart
@@ -29,6 +29,7 @@ class _HomeState extends State<Home> {
             subtitle: new Text("Detail One"),
             onTap: () => {},
           ),
+          new ListTile()
         ],
       ),
     );
