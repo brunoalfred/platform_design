76b24337f67960633c9c2adc9ea4e016f0d4e148
Bruno Alfred <alfredybruno@gmail.com>
2020-04-22 17:36:29 +0300

    item builder in the main
    Signed-off-by: Bruno Alfred <alfredybruno@gmail.com>

--- hello_flutter/offline/lib/views/home.dart
+++ hello_flutter/offline/lib/views/home.dart
@@ -13,16 +13,20 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: AppBar(
-        title: new Text(
-          "My App",
-          style: new TextStyle(
-            color: Colors.black,
+        appBar: AppBar(
+          title: new Text(
+            "My App",
+            style: new TextStyle(
+              color: Colors.black,
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: new ListView.builder(itemBuilder:,)
-    );
+        body: new ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return new Card();
+          },
+        ));
   }
 }
