3c94d996ef8f0f136d4fef3b3f0756e489f65365
Bruno Alfred <alfredybruno@gmail.com>
2020-04-23 21:44:03 +0300

    floating action button
    Signed-off-by: Bruno Alfred <alfredybruno@gmail.com>

--- hello_flutter/offline/lib/views/home.dart
+++ hello_flutter/offline/lib/views/home.dart
@@ -13,29 +13,35 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        appBar: AppBar(
-          title: new Text(
-            "My App",
-            style: new TextStyle(
-              color: Colors.blue,
-            ),
+      appBar: AppBar(
+        title: new Text(
+          "My App",
+          style: new TextStyle(
+            color: Colors.white,
           ),
-          centerTitle: true,
         ),
-        body: new ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return new Card(
-              child: ListTile(
-                leading: Icon(Icons.account_box),
-                title: new Text('Box'),
-                trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: () {
-                  Navigator.pushNamed(context, '/user');
-                },
-              ),
-            );
-          },
-        ));
+        centerTitle: true,
+      ),
+      body: new ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return new Card(
+            child: ListTile(
+              leading: Icon(Icons.account_box),
+              title: new Text('Box'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.pushNamed(context, '/user');
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/gesture');
+        },
+      ),
+    );
   }
 }
