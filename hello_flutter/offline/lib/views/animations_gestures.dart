d980837eb4122f5cd1354d74ffc7fd97efdff6d2
Bruno Alfred <alfredybruno@gmail.com>
2020-04-23 17:26:34 +0300

    creating the state full class
    Signed-off-by: Bruno Alfred <alfredybruno@gmail.com>

---
+++ hello_flutter/offline/lib/views/animations_gestures.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnimationsGestures extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _AnimationsGesturesState();
+  }
+}
+
+class _AnimationsGesturesState extends State<AnimationsGestures> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold();
+  }
+}
