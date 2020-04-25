b0b5a86894dbada4b12c415d0faf2b6880c1ef67
Bruno Alfred <alfredybruno@gmail.com>
2020-04-25 22:56:54 +0300

    creating the getters

--- hello_flutter/flutter_app/lib/models/todo.dart
+++ hello_flutter/flutter_app/lib/models/todo.dart
@@ -10,6 +10,12 @@ class Todo {
   Todo(this._title, this._priority, this._date, [this._description]);
   Todo.withId(this._id, this._title, this._priority, this._date, [this._description]);
 
+// * creating the getters for our class
+   int get id => _id;
+   String get title => _title;
+   String get description => _description;
+   String get date => _date;
+   String get priority => _priority;
 
 
 }
