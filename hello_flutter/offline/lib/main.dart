9cb9d8a72c1339e6f6881767643213589194aa76
Bruno Alfred <alfredybruno@gmail.com>
2020-04-25 21:43:57 +0300

    added myGesture widget to the route
    Signed-off-by: Bruno Alfred <alfredybruno@gmail.com>

--- hello_flutter/offline/lib/main.dart
+++ hello_flutter/offline/lib/main.dart
@@ -2,22 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:offline/views/gestures.dart';
 import 'package:offline/views/home.dart';
+import 'package:offline/views/myGesture.dart';
 import 'package:offline/views/user_view.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'Trying Navigation App With ListView',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.indigoAccent,
-        brightness: Brightness.light,
-      ),
+void main() => runApp(
+      MaterialApp(
+        title: 'Trying Navigation App With ListView',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.indigoAccent,
+          brightness: Brightness.light,
+        ),
 //      home: Home(),
 //  The first Route an individual takes
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
 //        Build the Home Screen,
-        '/': (context) => Home(),
-        '/user': (context) => User(),
-        '/gestures': (context) => AnimationsGestures()
-      },
-    ));
+          '/': (context) => Home(),
+          '/user': (context) => User(),
+          '/gestures': (context) => AnimationsGestures(),
+          'myGesture': (context) => myGesture(),
+        },
+      ),
+    );
