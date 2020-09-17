ecb3b89d7226a5d7db4daa04b6ec630fabfb7675
bruno alfred <alfredybruno@gmail.com>
2020-09-18 02:09:34 +0300

    create the home_screen

--- lib/tabs/all_tasks.dart
+++ lib/tabs/all_tasks.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:todos/providers/todos_model.dart';
-import 'package:todos/widgets/task_list.dart';
 
 class AllTasksTab extends StatelessWidget {
   @override
