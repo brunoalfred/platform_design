0a7cd1bdcb941aa4e397ba7b33a4cfa54c2ca541
bruno alfred <alfredybruno@gmail.com>
2020-09-18 02:09:16 +0300

    create :movie_camera: page

---
+++ lib/tabs/incomplete_tasks.dart
@@ -0,0 +1,19 @@
+// lib/tabs/incomplete_tasks.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:todos/providers/todos_model.dart';
+import 'package:todos/widgets/task_list.dart';
+
+class IncompleteTasksTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<TodosModel>(
+        builder: (context, todos, child) => TaskList(
+              tasks: todos.incompleteTasks,
+            ),
+      ),
+    );
+  }
+}
