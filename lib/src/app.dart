b4c7292d73cf3fccf15dd2fcd2ec565849b537d5
brunoalfred <alfredybruno@gmail.com>
2021-04-30 20:37:12 +0300

    material app/ entry for our application

---
+++ lib/src/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:platform_design/src/views/screens/HomePage.dart';
+
+
+class MyAdaptingApp extends StatelessWidget{
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Adaptive Music App',
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+    ),
+    darkTheme: ThemeData.dark(),
+    builder: (context, child){
+      return CupertinoTheme(data: CupertinoThemeData(), child: Material(child: child),);
+    },
+    home: HomePage(),
+  ) ;
+}
+
+
+
+
+}
