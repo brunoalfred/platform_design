c52267b426ab4fba15cc75e066e00263ddb0fa65
brunoalfred <alfredybruno@gmail.com>
2021-04-30 20:37:25 +0300

    created the news tab

---
+++ lib/src/views/components/news_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class NewsTab extends StatefulWidget {
+  static const title = 'News';
+  static const androidIcon = Icon(Icons.library_books);
+  static const iosIcon = Icon(CupertinoIcons.news);
+
+  
+  @override
+  _NewsTabState createState() => _NewsTabState();
+}
+
+class _NewsTabState extends State<NewsTab> {
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
