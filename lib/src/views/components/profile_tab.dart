1ef96b498271d73c8b2847869e8e53840e707559
brunoalfred <alfredybruno@gmail.com>
2021-04-30 20:37:39 +0300

    created the profile tab

---
+++ lib/src/views/components/profile_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class ProfileTab extends StatelessWidget {
+  
+    static const title = 'Profile';
+  static const androidIcon = Icon(Icons.person);
+  static const iosIcon = Icon(CupertinoIcons.profile_circled);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
