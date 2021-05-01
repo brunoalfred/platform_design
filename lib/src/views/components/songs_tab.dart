1915d45695829b5bce42deae063e87e5e07d6c20
brunoalfred <alfredybruno@gmail.com>
2021-05-01 23:33:15 +0300

    songs tab

--- lib/src/views/components/songs_tab.dart
+++ lib/src/views/components/songs_tab.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:platform_design/src/utils.dart';
+import 'package:platform_design/src/views/wigdets/hero_animating_song_card.dart';
+import 'package:platform_design/src/views/components/widgets.dart';
+import 'package:platform_design/src/views/components/song_detail_tab.dart';
 
 
 class SongsTab extends StatefulWidget {
-
   static const title = 'Songs';
   static const androidIcon = Icon(Icons.music_note);
   static const iosIcon = Icon(CupertinoIcons.music_note);
@@ -12,16 +16,155 @@ class SongsTab extends StatefulWidget {
 
   final Widget? androidDrawer;
 
-
   @override
   _SongsTabState createState() => _SongsTabState();
 }
 
 class _SongsTabState extends State<SongsTab> {
+  static const _itemsLength = 50;
+
+  final _androidRefreshKey = GlobalKey<RefreshIndicatorState>();
+
+  late List<MaterialColor> colors;
+  late List<String> songNames;
+
+   @override
+  void initState() {
+    _setData();
+    super.initState();
+  }
+
+
+  void _setData() {
+    colors = getRandomColors(_itemsLength);
+    songNames = getRandomNames(_itemsLength);
+  }
+
+  Future<void> _refreshData() {
+    return Future.delayed(
+      // This is just an arbitrary delay that simulates some network activity.
+      const Duration(seconds: 2),
+      () => setState(() => _setData()),
+    );
+  }
+
+
+  Widget _listBuilder(BuildContext context, int index) {
+    if (index >= _itemsLength) return Container();
+
+    // Show a slightly different color palette. Show poppy-ier colors on iOS
+    // due to lighter contrasting bars and tone it down on Android.
+    final color = defaultTargetPlatform == TargetPlatform.iOS
+        ? colors[index]
+        : colors[index].shade400;
+
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Hero(
+        tag: index,
+        child: HeroAnimatingSongCard(
+          song: songNames[index],
+          color: color,
+          heroAnimation: AlwaysStoppedAnimation(0),
+          onPressed: () => Navigator.of(context).push<void>(
+            MaterialPageRoute(
+              builder: (context) => SongDetailTab(
+                id: index,
+                song: songNames[index],
+                color: color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+   void _togglePlatform() {
+    TargetPlatform _getOppositePlatform() {
+      if (defaultTargetPlatform == TargetPlatform.iOS) {
+        return TargetPlatform.android;
+      } else {
+        return TargetPlatform.iOS;
+      }
+    }
+
+    debugDefaultTargetPlatformOverride = _getOppositePlatform();
+    // This rebuilds the application. This should obviously never be
+    // done in a real app but it's done here since this app
+    // unrealistically toggles the current platform for demonstration
+    // purposes.
+    WidgetsBinding.instance!.reassembleApplication();
+  }
+
+
+
+    Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(SongsTab.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () async =>
+                await _androidRefreshKey.currentState!.show(),
+          ),
+          IconButton(
+            icon: Icon(Icons.shuffle),
+            onPressed: _togglePlatform,
+          ),
+        ],
+      ),
+      drawer: widget.androidDrawer,
+      body: RefreshIndicator(
+        key: _androidRefreshKey,
+        onRefresh: _refreshData,
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 12),
+          itemCount: _itemsLength,
+          itemBuilder: _listBuilder,
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _buildIos(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          trailing: CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: Icon(CupertinoIcons.shuffle),
+            onPressed: _togglePlatform,
+          ),
+        ),
+        CupertinoSliverRefreshControl(
+          onRefresh: _refreshData,
+        ),
+        SliverSafeArea(
+          top: false,
+          sliver: SliverPadding(
+            padding: EdgeInsets.symmetric(vertical: 12),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                _listBuilder,
+                childCount: _itemsLength,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return PlatformWidget(
+androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    ) ;
   }
 }
