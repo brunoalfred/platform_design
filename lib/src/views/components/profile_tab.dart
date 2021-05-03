960bc7838edca81fd2132214476b5acd8ef7b12f
brunoalfred <alfredybruno@gmail.com>
2021-05-03 13:30:22 +0300

    Added choices widget to the widgets file

--- lib/src/views/components/profile_tab.dart
+++ lib/src/views/components/profile_tab.dart
@@ -1,16 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:platform_design/src/views/components/widgets.dart';
+import 'package:platform_design/src/views/components/settings_tab.dart';
+import 'package:platform_design/src/views/wigdets/preference_card.dart';
+import 'package:platform_design/src/views/wigdets/log_out_button.dart';
 
 class ProfileTab extends StatelessWidget {
-  
-    static const title = 'Profile';
+  static const title = 'Profile';
   static const androidIcon = Icon(Icons.person);
   static const iosIcon = Icon(CupertinoIcons.profile_circled);
-  
+
+  Widget _buildBody(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Center(
+              child: Text(
+                '😼',
+                style: TextStyle(
+                  fontSize: 80,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ),
+          ),
+          PreferenceCard(
+            header: 'MY INTENSITY PREFERENCE',
+            content: '🔥',
+            preferenceChoices: [
+              'Super heavy',
+              'Dial it to 11',
+              "Head bangin'",
+              '1000W',
+              'My neighbor hates me',
+            ],
+          ),
+          PreferenceCard(
+            header: 'CURRENT MOOD',
+            content: '🤘🏾🚀',
+            preferenceChoices: [
+              'Over the moon',
+              'Basking in sunlight',
+              'Hello fellow Martians',
+              'Into the darkness',
+            ],
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          LogOutButton(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+          trailing: CupertinoButton(
+        padding: EdgeInsets.zero,
+        child: SettingsTab.iosIcon,
+        onPressed: () {
+          // This pushes the settings page as a full page modal dialog on top
+          // of the tab bar and everything.
+          Navigator.of(context, rootNavigator: true).push<void>(
+            CupertinoPageRoute(
+              title: SettingsTab.title,
+              fullscreenDialog: true,
+              builder: (context) => SettingsTab(),
+            ),
+          );
+        },
+      )),
+      child: _buildBody(context),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
     );
   }
 }
--- lib/src/views/components/widgets.dart
+++ lib/src/views/components/widgets.dart
@@ -29,3 +29,78 @@ class PlatformWidget extends StatelessWidget {
     }
   }
 }
+
+
+
+void showChoices(BuildContext context, List<String> choices) {
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      showDialog<void>(
+        context: context,
+        builder: (context) {
+          int? selectedRadio = 1;
+          return AlertDialog(
+            contentPadding: EdgeInsets.only(top: 12),
+            content: StatefulBuilder(
+              builder: (context, setState) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: List<Widget>.generate(choices.length, (index) {
+                    return RadioListTile<int?>(
+                      title: Text(choices[index]),
+                      value: index,
+                      groupValue: selectedRadio,
+                      onChanged: (value) {
+                        setState(() => selectedRadio = value);
+                      },
+                    );
+                  }),
+                );
+              },
+            ),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: Text('CANCEL'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    case TargetPlatform.iOS:
+      showCupertinoModalPopup<void>(
+        context: context,
+        builder: (context) {
+          return SizedBox(
+            height: 250,
+            child: CupertinoPicker(
+              backgroundColor: Theme.of(context).canvasColor,
+              useMagnifier: true,
+              magnification: 1.1,
+              itemExtent: 40,
+              scrollController: FixedExtentScrollController(initialItem: 1),
+              children: List<Widget>.generate(choices.length, (index) {
+                return Center(
+                  child: Text(
+                    choices[index],
+                    style: TextStyle(
+                      fontSize: 21,
+                    ),
+                  ),
+                );
+              }),
+              onSelectedItemChanged: (value) {},
+            ),
+          );
+        },
+      );
+      return;
+    default:
+      assert(false, 'Unexpected platform $defaultTargetPlatform');
+  }
+}
